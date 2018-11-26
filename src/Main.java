fd3778b7d75d67101f5a9d3ff89ef2bfe23be7f5
Ayaka Toma <e175737@ie.u-ryukyu.ac.jp>
2018-11-27 00:17:17 +0900

    例外処理

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 public class Main {
 
-    public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
-
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました");
+            System.out.println(e.getMessage());
+        }
     }
 }
